9db89e72dd07aa16bea7f54d52303b0f22f67b15
Vlad7Stepanov7 <svlad4117@gmail.com>
2023-05-01 18:35:18 +0300

    fixed basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="Vlad7Stepanov7/test-task-tweets" >
+    <BrowserRouter basename="/test-task-tweets" >
       <App />
     </BrowserRouter>
   </React.StrictMode>
